7b0c4ed4a8b69b7a5fb2901a733bfdc471333a9d
xschen <xs0040@gmail.com>
2017-09-10 22:01:43 +0800

    minor update

--- test/MagentoAccountApi.spec.js
+++ test/MagentoAccountApi.spec.js
@@ -2,8 +2,6 @@ import {MagentoAccountApi} from '../build/MagentoApi';
 import MagentoAccountInfo from './MagentoAccountInfo';
 import expect from 'expect';
 
-const defaultUrl = 'http://j-clef-web-01.japaneast.cloudapp.azure.com';
-const username = ''
 
 describe('Magento Account Api', () => {
   it('should return a valid token if given a valid client username and password', function(done) {
